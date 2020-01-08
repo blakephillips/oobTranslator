0e8766cd19956b8182a298d924a4de9ba3d87297
Blake Phillips <blake.a.phillips3@gmail.com>
2020-01-08 15:11:34 -0500

    Add project files.

---
+++ oobTranslator/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Speech.Synthesis;
+
+namespace oobTranslator
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        oobClass oob = new oobClass();
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtOutput.Text = oob.oobifyString(txtInput.Text);
+        }
+
+        private void ReaderClick(object sender, RoutedEventArgs e)
+        {
+            reader.SpeakAsync(txtOutput.Text);
+        }
+    }
+}
---
+++ oobTranslator/oobClass.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oobTranslator
+{
+    class oobClass
+    {
+        /// <summary>
+        /// Returns an oobified string
+        /// </summary>
+        /// <param name="input">Un-oobified input</param>
+        /// <returns>An oobified string</returns>
+        public string oobifyString(string input)
+        {
+            StringBuilder output = new StringBuilder();
+            string vowels = "aeiou";
+            foreach (char c in input)
+            {
+                bool isUpper = Char.IsUpper(c);
+                char myC = Char.ToLower(c);
+
+                bool isOob = false;
+
+                foreach(char x in vowels)
+                {
+                    if (myC == x)
+                    {
+                        isOob = true;
+                    }
+                }
+                if (isOob)
+                {
+                    if (isUpper)
+                    {
+                        output.Append(new char[] { 'O' });
+                    } else
+                    {
+                        output.Append(new char[] { 'o' });
+                    }
+                    output.Append(new char[] { 'o', 'b' });
+                } else
+                {
+                    output.Append(c);
+                }
+
+            }
+            return output.ToString(); 
+        }
+    }
+}
